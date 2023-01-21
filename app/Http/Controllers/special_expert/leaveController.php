890e84965ae8ce86803eeee64ebd923fb402f636
sara <sarajafary26@gmail.com>
2023-01-21 14:14:31 +0330

    creat special_expert role

---
+++ app/Http/Controllers/special_expert/leaveController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\special_expert;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class leaveController extends Controller
+{
+    //
+}
