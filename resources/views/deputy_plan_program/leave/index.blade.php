521da1b2f84a9858641de3b15de45e18d9556f50
sara <sarajafary26@gmail.com>
2023-01-21 10:18:43 +0330

    change the line 35

--- resources/views/deputy_plan_program/leave/index.blade.php
+++ resources/views/deputy_plan_program/leave/index.blade.php
@@ -32,7 +32,7 @@
                                         <table class="table table-hover table-custom spacing8 text-center">
                                             <thead>
                                             <tr>
-                                                <th>#</th>
+                                                <th>$</th>
                                                 <th>نوع مرخصی</th>
                                                 <th>از روز - ساعت</th>
                                                 <th>تا روز - ساعت</th>
