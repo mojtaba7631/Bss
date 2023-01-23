69eae982bf6e542adb9de27fafcf1d50c6b75a18
sara <sarajafary26@gmail.com>
2023-01-23 12:35:40 +0330

    fix status

--- app/Http/Controllers/adjustment_expert/leaveController.php
+++ app/Http/Controllers/adjustment_expert/leaveController.php
@@ -173,7 +173,6 @@ class leaveController extends Controller
 
         $leave_info->update([
             'main_manager_approval' => 1,
-            'status'=>4,
         ]);
 
         return response()->json([
